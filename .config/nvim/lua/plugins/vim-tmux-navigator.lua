496665dcf36b5a1f445d6e2cec4bb319f316273b
Gaetan Puleo <pro@gaetanpuleo.com>
2023-09-24 08:38:13 +0200

    chore: remove nvim

--- .config/nvim/lua/plugins/vim-tmux-navigator.lua
+++
@@ -1,7 +0,0 @@
-local map = vim.keymap.set
-vim.g.tmux_navigator_no_mappings = 1
-
-map({'n'}, '<M-h>', ':<C-U>TmuxNavigateLeft<cr>', {noremap = true, silent = true})
-map({'n'}, '<M-l>', ':<C-U>TmuxNavigateRight<cr>', {noremap = true, silent = true})
-map({'n'}, '<M-k>', ':<C-U>TmuxNavigateUp<cr>', {noremap = true, silent = true})
-map({'n'}, '<M-j>', ':<C-U>TmuxNavigateDown<cr>', {noremap = true, silent = true})
